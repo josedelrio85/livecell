d5ce9ecc6bfa1e0266fe5a2f308fed1f2d924ffc
Jose del Río <joserio@bysidecar.com>
2019-12-18 15:41:15 +0100

    Added capability of return response from goroutine using channel properties

--- pkg/api.go
+++ pkg/api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/schema"
 )
@@ -23,13 +24,6 @@ type Client struct {
 func (c *Client) HandleFunction() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		keys := r.URL.Query()
-
-		// TODO remove it
-		for i, k := range keys {
-			log.Printf("key %v => %s", keys[i], k)
-		}
-
 		c.Live = LeadLive{}
 		err := decoder.Decode(&c.Live, r.URL.Query())
 		if err != nil {
@@ -39,42 +33,13 @@ func (c *Client) HandleFunction() http.Handler {
 			return
 		}
 
+		log.Printf("status OK => %d - %s", c.Live.SmartcenterID, time.Now().Format("2006-01-02 15:04:05"))
 		w.WriteHeader(http.StatusOK)
 
-		go c.process()
+		c.process()
+		// result := c.process()
+		// result.printout()
 	})
-}
-
-// process function tries to recover missing data and store Live Lead into DB
-func (c *Client) process() {
-	if c.Live.QueueID > 0 {
-		if err := c.getValues(); err != nil {
-			msg := "Error retrieving Queue registry"
-			e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
-			e.sendAlarm()
-		}
-	}
-
-	// TODO remove it
-	log.Printf("Live SouID %d", c.Live.SouID)
-	log.Printf("Live TypeID %d", c.Live.TypeID)
-
-	if c.Live.SouID == 0 || c.Live.TypeID == 0 {
-		msg := "Error retrieving source and type values"
-		err := fmt.Errorf("%s", msg)
-		e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
-		e.sendAlarm()
-		return
-	}
-
-	if err := c.Storer.Insert(&c.Live); err != nil {
-		msg := "Error inserting live lead"
-		e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
-		e.sendAlarm()
-	}
-
-	// TODO remove it
-	log.Println("done...")
 }
 
 // getValues is a function to retrieve the souid and typeid values from queueid
@@ -91,4 +56,53 @@ func (c *Client) getValues() error {
 	live.TypeID = queue.QueType
 
 	return nil
+}
+
+// ResultError is a struct used to return a response from a goroutine using a channel
+type ResultError struct {
+	res string
+	err error
+}
+
+// process function tries to recover missing data and store Live Lead into DB
+// uses a goroutine returning ResultError struct throw a channel
+func (c *Client) process() ResultError {
+	outputChannel := make(chan ResultError)
+
+	go func() {
+		if c.Live.QueueID > 0 {
+			if err := c.getValues(); err != nil {
+				msg := "Error retrieving Queue registry"
+				e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
+				e.sendAlarm()
+				outputChannel <- ResultError{res: "test", err: err}
+			}
+		}
+
+		if c.Live.SouID == 0 || c.Live.TypeID == 0 {
+			msg := "Error retrieving source and type values"
+			err := fmt.Errorf("%s", msg)
+			e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
+			e.sendAlarm()
+			outputChannel <- ResultError{res: "test2", err: err}
+		}
+
+		if err := c.Storer.Insert(&c.Live); err != nil {
+			msg := "Error inserting live lead"
+			e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
+			e.sendAlarm()
+			outputChannel <- ResultError{res: "test3", err: err}
+		}
+		outputChannel <- ResultError{res: "test4", err: nil}
+	}()
+	return <-outputChannel
+}
+
+// printout function prints the data returned from the goroutine
+// used as example for future cases, not used in production environment
+func (r *ResultError) printout() {
+	if r.err != nil {
+		fmt.Printf("Failed: %s\n", r.err.Error())
+	}
+	fmt.Printf("Name: \"%s\" has occurred \n", r.res)
 }
