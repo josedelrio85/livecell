497afecec108e763712864ff9c02abbaef7ed4e6
Jose del Río <joserio@bysidecar.com>
2020-02-20 09:40:29 +0100

    restructuration of leadlive model

--- pkg/model_leadlive.go
+++ pkg/model_leadlive.go
@@ -4,47 +4,41 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+// LeadPayload represents the received data structure from Leontel environment
+// Equivalences to get values from Leontel
+// lea_id => leadId		|| 	cat => cat_id
+// subcat => sub_id  	|| 	queue => queId
+// phone => TELEFONO 	|| 	url => url
+// is_client => cli 	|| 	ws_id => wsid
+// ord_id => isOpenOrder
+type LeadPayload struct {
+	QueueID       int64   `schema:"queue"`
+	SmartcenterID int64   `schema:"lea_id"`
+	CatID         int64   `schema:"cat"`
+	SubcatID      int64   `schema:"subcat"`
+	OrdID         *string `schema:"ord_id,omitempty"` // {{
+	Wsid          *string `schema:"ws_id,omitempty"`  // {{
+	Phone         *string `schema:"phone"`
+	URL           *string `schema:"url"` // {{
+	IsClient      bool    `schema:"is_client"`
+}
+
 // LeadLive represents the structure of Live Lead entity
 type LeadLive struct {
 	gorm.Model
-	SmartcenterID int64   `schema:"lea_id"`
-	SouID         int64   `schema:"-"`
-	TypeID        int64   `schema:"-"`
-	CatID         int64   `schema:"cat"`
-	SubcatID      int64   `schema:"subcat"`
-	QueueID       int64   `schema:"queue"`
-	CliID         int64   `schema:"cli"`
-	User          int64   `schema:"user,omitempty"`
-	Closed        int64   `schema:"closed,omitempty"`
-	Phone         *string `schema:"phone,omitempty"`
-	Name          *string `schema:"name,omitempty"`
-	Surname       *string `schema:"surn1,omitempty"`
-	SecondSurname *string `schema:"surn2,omitempty"`
-	DNI           *string `schema:"dni,omitempty"`
-	Obs           *string `sql:"type:text" schema:"obs,omitempty"`
-	FullName      *string `schema:"fullname,omitempty"`
-	URL           *string `sql:"type:text" schema:"url,omitempty"`
-	Wsid          int64   `schema:"wsid,omitempty"`
-	IP            *string `schema:"ip,omitempty"`
-	Mail          *string `schema:"mail,omitempty"`
-
-	OrdID  int64 `schema:"ord_id,omitempty"`
-	OrdSub int64 `schema:"ord_sub,omitempty"`
-
-	ClientType    *string `schema:"clienttyp,omitempty"`
-	Town          *string `schema:"town,omitempty"`
-	State         *string `schema:"state,omitempty"`
-	Street        *string `schema:"street,omitempty"`
-	StreetAlt     *string `schema:"street2,omitempty"`
-	PostalCode    *string `schema:"cp,omitempty"`
-	Number        *string `schema:"number,omitempty"`
-	FiberCompany  *string `schema:"fibercomp,omitempty"`
-	MobileCompany *string `schema:"mobilecomp,omitempty"`
-
-	// SouIDLeontel       int64      `sql:"-" schema:"sou_id_leontel"`
-	// SouDescLeontel     string     `sql:"-" schema:"sou_desc_leontel"`
-	// LeatypeIDLeontel   int64      `sql:"-" schema:"lea_type_leontel"`
-	// LeatypeDescLeontel string     `sql:"-" schema:"lea_type_desc_leontel"`
+	SouID         int64
+	TypeID        int64
+	QueueID       int64
+	SmartcenterID int64
+	CatID         int64
+	SubcatID      int64
+	OrdID         int64
+	Wsid          int64
+	Closed        int64
+	Phone         *string
+	IsClient      bool
+	URL           *string
+	// User   int64 `schema:"user,omitempty"`
 }
 
 // TableName sets the default table name
